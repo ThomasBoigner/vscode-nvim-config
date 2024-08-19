12e82a6ffbd34385b560cdbad4cfcfb17dd02dda
Thomas Boigner <thomas.boigner@bytesource.net>
2024-08-19 10:26:58 +0200

    Add checks to after folder to prevent crashes when opening vscode

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,10 +1,13 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
-  sync_install = false,
-  auto_install = true,
+if not vim.g.vscode then
+    require('nvim-treesitter.configs').setup {
+        ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+        sync_install = false,
+        auto_install = true,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+    }
+end
+
